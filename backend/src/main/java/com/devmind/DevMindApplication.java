edc595455d7e7c67a457030b7f8e38cbc303b090
wty <tywang.dev@outlook.com>
2026-07-01 00:19:39 +0800

    fix: restrict MyBatis mapper scanning

--- backend/src/main/java/com/devmind/DevMindApplication.java
+++ backend/src/main/java/com/devmind/DevMindApplication.java
@@ -1,11 +1,12 @@
 package com.devmind;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.devmind.module")
+@MapperScan(basePackages = "com.devmind.module", markerInterface = BaseMapper.class)
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class DevMindApplication {
 
